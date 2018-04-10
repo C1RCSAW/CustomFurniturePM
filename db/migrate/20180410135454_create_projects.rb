e7d04486ceeb2330bb96b85952a02a12f259afa4
Brian Bytnar <bytnarb2@gmail.com>
2018-04-10 17:10:58 -0400

    stubbed out user and project model functionality and added attributes to corresponding migrations.

--- db/migrate/20180410135454_create_projects.rb
+++ db/migrate/20180410135454_create_projects.rb
@@ -1,4 +1,11 @@
 class CreateProjects < ActiveRecord::Migration[5.1]
   def change
+    create_table :projects do |t|
+      t.string :furniture_type #ex chair, table, bench, facia, etc.
+      t.string :material
+      t.string :cost_to_produce
+      t.string :price_to_client
+      t.string :status
+      t.integer :user_id
   end
 end
