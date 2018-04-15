ebc1bf07d816a60c557e7bcba2f03afe45b5cf69
Brian Bytnar <bytnarb2@gmail.com>
2018-04-15 11:26:45 -0500

    changed cost and price attributes in table from string to float.

--- db/migrate/20180410135454_create_projects.rb
+++ db/migrate/20180410135454_create_projects.rb
@@ -3,8 +3,8 @@ class CreateProjects < ActiveRecord::Migration[5.1]
     create_table :projects do |t|
       t.string :furniture_type #ex chair, table, bench, facia, etc.
       t.string :material
-      t.string :cost_to_produce
-      t.string :price_to_client
+      t.float :cost_to_produce
+      t.float :price_to_client
       t.string :status
       t.integer :user_id
   end
