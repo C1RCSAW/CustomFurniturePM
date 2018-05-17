73b59cec16b27fa987e1a1e04d82d730bc2305f6
Brian Bytnar <bytnarb2@gmail.com>
2018-05-17 10:06:13 -0400

    added validations to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
   has_many :projects, through: :clients ## ? => @maker.client.projects ?
   has_secure_password
 
+  validates :username, :email, presence: true
+  validates_uniqueness_of :username
+
   def slug
     self.username.downcase.gsub(" ", "-")
   end
