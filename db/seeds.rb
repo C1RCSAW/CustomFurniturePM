9c40ef7672038c6810f25beae198eff059b0bb87
Brian Bytnar <bytnarb2@gmail.com>
2018-05-19 15:42:53 -0400

    added costs array to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,21 @@ clients.each do |client|
   Client.create(client)
 end
 
-projects = [{client_id: Client.first.id, name: 'Staked Leg Coffee Table', furniture_type: 'table', material: 'oak and pine', cost_to_produce: 100, price_to_client: 250, status: 'complete'},
-            {client_id: Client.second.id, name: 'Modern Lounger', furniture_type: 'chair', material: 'aluminum and fabric', cost_to_produce: 800, price_to_client: 1600, status: 'in production'},
-            {client_id: Client.second.id, name: 'Applebox Inspired Desk', furniture_type: 'desk', material: 'plywood and steel', cost_to_produce: 160, price_to_client: 300, status: 'estimate sent to client'}]
+projects = [{client_id: Client.first.id, name: 'Staked Leg Coffee Table', furniture_type: 'table', material: 'oak and pine', price_to_client: 250, status: 'complete'},
+            {client_id: Client.second.id, name: 'Modern Lounger', furniture_type: 'chair', material: 'aluminum and fabric', price_to_client: 1600, status: 'in production'},
+            {client_id: Client.second.id, name: 'Applebox Inspired Desk', furniture_type: 'desk', material: 'plywood and steel', price_to_client: 400, status: 'estimate sent to client'}]
 
 projects.each do |project|
   Project.create(project)
+end
+
+costs = [{project_id: Project.first.id, description: 'materials', amount: 50},
+         {project_id: Project.first.id, description: 'labor', amount: 100},
+         {project_id: Project.second.id, description: 'materials', amount: 200},
+         {project_id: Project.second.id, description: 'labor', amount: 600},
+         {project_id: Project.third.id, description: 'materials', amount: 100},
+         {project_id: Project.third.id, description: 'labor', amount: 100}]
+
+costs.each do |cost|
+  Cost.create(cost)
 end
