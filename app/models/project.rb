7d80fe58599ee41d88df32ea6b2c86b84ec570ac
Brian Bytnar <bytnarb2@gmail.com>
2018-05-19 16:11:54 -0400

    added has_many :costs relationship to Project class.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,12 @@
 class Project < ActiveRecord::Base
   belongs_to :client ## => @project.client ## has embedded validation
+  has_many :costs ## => @project.costs
+
+  def total_cost
+    self.costs each do |amount|
+  end
 
   def profit
-    self.price_to_client - self.cost_to_produce
+    self.price_to_client - self.total_cost
   end
 end
