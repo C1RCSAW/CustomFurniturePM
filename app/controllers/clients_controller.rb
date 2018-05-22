0e753c77ba8eeefbfe8e42e603159719b7e9e8bc
Brian Bytnar <bytnarb2@gmail.com>
2018-05-22 12:27:35 -0400

    added show routes, controller actions, and views for clients and projects.

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,3 +1,11 @@
 class ClientsController < ApplicationController
   # Ensure that the belongs_to resource has routes for Creating, Reading, Updating and Destroying
+  get '/clients/:id' do
+    if logged_in?
+      @client = Client.find_by_id(params[:id])
+      erb :'clients/show_client'
+    else
+      redirect to '/login'
+    end
+  end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,3 +1,12 @@
 class ProjectsController < ApplicationController
   # Ensure that the belongs_to resource has routes for Creating, Reading, Updating and Destroying
+  get '/projects/:id' do
+    if logged_in?
+      @client = Client.find_by_id(params[:id])
+      @project = Project.find_by_id(params[:id])
+      erb :'projects/show_project'
+    else
+      redirect to '/login'
+    end
+  end
 end
