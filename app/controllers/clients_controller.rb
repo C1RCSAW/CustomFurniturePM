78121027ef26fbe379db7c3cc6705c26bea50a32
Brian Bytnar <bytnarb2@gmail.com>
2018-05-25 12:15:42 -0400

    added clients/new route/action to clients_controller.rb.

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,19 +1,29 @@
 class ClientsController < ApplicationController
   # Ensure that the belongs_to resource has routes for Creating, Reading, Updating and Destroying
-  get '/clients/:id' do
-    if logged_in?
-      @client = Client.find_by_id(params[:id])
-      erb :'clients/show'
-    else
-      redirect to '/login'
-    end
-  end
 
   get '/clients' do
     if logged_in?
       @user = User.find(current_user.id)
       @client = Client.find_by_id(params[:id])
       erb :'clients/index'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/clients/new' do
+    if logged_in?
+      @user = User.find(current_user.id)
+      erb :'clients/new'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/clients/:id' do
+    if logged_in?
+      @client = Client.find_by_id(params[:id])
+      erb :'clients/show'
     else
       redirect to '/login'
     end
