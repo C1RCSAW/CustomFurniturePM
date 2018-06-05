c377e849bdd64356d444f85a4d514ea18985593c
Brian Bytnar <bytnarb2@gmail.com>
2018-06-05 12:45:50 -0400

    experimented with validations using regular statements in Client class model vs html input class type email validation in the clients/new form.

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,9 @@
 class Client < ActiveRecord::Base
   belongs_to :user ## => @client.user
   has_many :projects ## => @client.projects
+
+  validates :name, presence: true
+
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, message: 'must be in the form of an email' }
+
 end
