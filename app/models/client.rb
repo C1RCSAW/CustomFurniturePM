5b2bb5ac75ab5c70641bada04d9172bc6af0c782
Brian Bytnar <bytnarb2@gmail.com>
2018-06-06 12:50:52 -0400

    attempt to add validation error messages to clients/new view.

--- app/models/client.rb
+++ app/models/client.rb
@@ -7,6 +7,8 @@ class Client < ActiveRecord::Base
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
   validates :phone_number, format: { with: /\(?([0-9]{3})\)?([ .-]?)(\1)\2([0-9]{4})/ }
 
+  #access errors through errors.messages
+
     # (?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})
 
 end
